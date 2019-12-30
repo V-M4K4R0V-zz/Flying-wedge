152cc418a6cad6aee8422418f08564affa2d639c
V-M4K4R0V <ahmedmontassir0@outlook.com>
2019-12-30 14:17:27 +0100

    motherland

---
+++ что такое столица/motherland.py
@@ -0,0 +1,6 @@
+print("privyet!! Comrade")
+usr = input("your name : ")
+R_SPY = open('russian_spys.txt', 'r')
+print("You know what ill call you " + R_SPY )
+print("SO! my друг {} what country  u want to invade today ?".format(R_SPY))
+
