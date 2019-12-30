bf998addd70031e9c4898bb5732f309d5a1de586
V-M4K4R0V <ahmedmontassir0@outlook.com>
2019-12-30 14:39:35 +0100

    motherland

--- что такое столица/motherland.py
+++ что такое столица/motherland.py
@@ -2,5 +2,8 @@ print("privyet!! Comrade")
 usr = input("your name : ")
 R_SPY = open('russian_spys.txt', 'r')
 print("You know what ill call you " + R_SPY )
-print("SO! my друг {} what country  u want to invade today ?".format(R_SPY))
-
+path = input("is it okay or not okay my друг [YES]or[NO] : ")
+if(path == 'NO'):
+    print("SYKAA!! GET OUR U FUCKING WESTERN SPY")
+else:
+    print("SO! my друг {} what country  u want to invade today ?".format(R_SPY))
