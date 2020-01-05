547890909f40003cc2ad9a49ba7ab0386856655c
V-M4K4R0V <ahmedelmountassir10@gmail.com>
2020-01-05 17:17:05 +0100

    papa-stalina

--- что такое столица/motherland.py
+++ что такое столица/motherland.py
@@ -8,99 +8,37 @@ R_SPY = open('russian_spys.txt', 'r')
 
 print("You know what ill call you " + R_SPY)
 
-path = input("is it okay to call you" + R_SPY + "my друг [YES]or[NO] : ").upper
+path = input("is it okay to call you my FKHIEND [YES]or[NO] : ").upper
 
 if(path == 'NO'):
     print("SYKAA!! GET OUT U FUCKING WESTERN SPY")
 elif(path == 'YES'):
-    print("SO! my друг what country  u want to invade today ?")
+    print("SO! my FKHIEND what country  u want to invade today ?")
     enemy = input("choose your enemy :").lower
-    def switch_demo(enemy):
-    switcher = {
-        morocco: "those are our друг's",
-        russia: "SYYYKA! you are a fucking traitor KGB ant to know ur location",
-        Afghanistan: ""
-        Albania: ""
-        Algeria: ""
-        Andorra: ""
-        Angola: ""
-        Antigua: ""
-        Argentina: ""
-        Armenia: ""
-        Australia: ""
-        Austria: ""
-        Azerbaijan: ""
-        The Bahamas: ""
-        Bahrain: ""
-        Bangladesh: ""
-        Barbados: ""
-        Belarus: ""
-        Belgium: ""
-        Belize: ""
-        Benin: ""
-        Bhutan: ""
-        Bolivia: ""
-        Bosnia: ""
-        Botswana: ""
-        Brazil: ""
-        Brunei: ""
-        Bulgaria: ""
-        Burkina Faso: ""
-        Burundi: ""
-        Cabo Verde: ""
-        Cambodia: ""
-        Cameroon: ""
-        Canada: ""
-        Central African Republic: ""
-        Chad: ""
-        Chile: ""
-        China: ""
-        Colombia: ""
-        Comoros: ""
-        Congo, Democratic Republic of the: ""
-        Congo, Republic of the: ""
-        Costa Rica: ""
-        Côte d’Ivoire: ""
-        Croatia: ""
-        Cuba: ""
-        Cyprus: ""
-        Czech Republic: ""
-        Denmark: ""
-        Djibouti: ""
-        Dominica: ""
-        Dominican Republic: ""
-        Ecuador: ""
-        Egypt: ""
-        El Salvador: ""
-        Equatorial Guinea: ""
-        Eritrea: ""
-        Estonia: ""
-        Eswatini: ""
-        Ethiopia: ""
-        Fiji: ""
-        Finland: ""
-        France: ""
-        Gabon: ""
-        The Gambia: ""
-        Georgia: ""
-        Germany: ""
-        Ghana: ""
-        Greece: ""
-        Grenada: ""
-        Guatemala: ""
-        Guinea: ""
-        Guyana: ""
-        Haiti: ""
-        Honduras: ""
-        Iceland: ""
-        India: ""
-        Indonesia: ""
-        Iran: ""
-        Iraq: ""
-        Ireland: ""
-        Israel: ""
-        Italy: ""
+
+    if(enemy == morroco):
+         "those are our FKHIEND's u shouldnt attack them",
+    elif(enemy == russia):
+         "SYYYKA! you are a fucking traitor KGB want to know ur location",
+    elif(enemy == Afghanistan):
+         "kha-kha-kha!ooh those freaking terrorists u'd better shoot them with the votkalachnikov"
+    elif(enemy ==  Albania):
+    elif(enemy ==  Algeria):
+    elif(enemy == Australia):
+    elif(enemy == Brazil):
+    elif(enemy == Canada):
+    elif(enemy == China):
+    elif(enemy == Colombia):
+    elif(enemy == Cuba):
+    elif(enemy == Czech):
+    elif(enemy == Egypt): 
+    elif(enemy == France):
+    elif(enemy == Germany):
+    elif(enemy == India):
+    elif(enemy == Iran):
+    elif(enemy == Iraq):
+    elif(enemy == Israel):
+    elif(enemy == Italy):
     }
-    print switcher.get(enemy, "Invalid month")
-else:
+    else:
     print("you fucking donkey i said yes or no not" + path)
