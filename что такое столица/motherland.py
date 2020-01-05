e308858bf948721dc861bcff0310f33894fad29a
V-M4K4R0V <ahmedelmountassir10@gmail.com>
2020-01-05 15:30:16 +0100

    papa-stalina

--- что такое столица/motherland.py
+++ что такое столица/motherland.py
@@ -1,14 +1,19 @@
+#author : V-M4K4R0V
+
 print("privyet!! Comrade")
 
 usr = input("your name : ")
+
 R_SPY = open('russian_spys.txt', 'r')
 
-print("You know what ill call you ")
+print("You know what ill call you " + R_SPY)
 
-path = input("is it okay or not okay my друг [YES]or[NO] : ")
+path = input("is it okay to call you" + R_SPY + "my друг [YES]or[NO] : ").upper
 
 if(path == 'NO'):
-    print("SYKAA!! GET OUR U FUCKING WESTERN SPY")
+    print("SYKAA!! GET OUT U FUCKING WESTERN SPY")
 elif(path == 'YES'):
     print("SO! my друг what country  u want to invade today ?")
-    enemy = input("choose your enemy :")
+    enemy = input("choose your enemy :").lower
+else:
+    print("you fucking donkey i said yes or no not" + path)
