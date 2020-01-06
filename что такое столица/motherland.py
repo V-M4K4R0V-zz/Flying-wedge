fb170b57767a88c4e465e511d9decef6888fa9c6
V-M4K4R0V <ahmedelmountassir10@gmail.com>
2020-01-06 16:22:34 +0100

    file-h

--- что такое столица/motherland.py
+++ что такое столица/motherland.py
@@ -1,11 +1,53 @@
 #author : V-M4K4R0V
 
 print("privyet!! Comrade")
-usr = input("your name : ")
+user = input("enter your name : ")
 
-f = open("russian_spys.txt", "r")
-
-if f.mode == 'r':
-     R_SPY =f.read()
 print("You know what ill call you " + R_SPY)
 path = input("is it okay to call you my FKHIEND [YES]or[NO] : ").upper
+
+if(path == 'NO'):
+     print("SYKAA!! GET OUT U FUCKING WESTERN SPY")
+elif(path == 'YES'):
+     print("SO! my FKHIEND what country  u want to invade today ?")
+     enemy = input("choose your enemy :").lower
+     if(enemy == 'morroco'):
+          print("those are our FKHIEND's u shouldnt attack them")
+     elif(enemy == 'russia'):
+          print("SYYYKA! you are a fucking traitor KGB want to know ur location")
+     elif(enemy == 'Afghanistan'):
+          print("kha-kha-kha!ooh those freaking terrorists u'd better shoot them with the votkalachnikov")
+     elif(enemy == 'Albania'):
+          print("hello")
+     elif(enemy == 'Australia'):
+          print("hi")
+     elif(enemy == 'Canada'):
+
+     elif(enemy == 'China'):
+
+     elif(enemy == 'Colombia'):
+
+     elif(enemy == 'Cuba'):
+
+     elif(enemy == 'Czech'):
+
+     elif(enemy == 'Egypt'):
+
+     elif(enemy == 'France'):
+
+     elif(enemy == 'Germany'):
+
+     elif(enemy == 'India'):
+
+     elif(enemy == 'Iran'):
+
+     elif(enemy == 'Iraq'):
+
+     elif(enemy == 'Israel'):
+
+     elif(enemy == 'Italy'):
+
+     else:
+          print("hello")       
+else:
+     print("you fucking donkey i said yes or no not" + path)
