20599b13e1c56c1df98c8172c184826a847b3366
V-M4K4R0V <ahmedelmountassir10@gmail.com>
2020-01-08 14:07:25 +0100

    function-call

--- mozer racha/motherland.py
+++ mozer racha/motherland.py
@@ -1,19 +1,17 @@
 #author : V-M4K4R0V
 #add music , graphics , national anthem
 #more dialogs
-from PIL import Image, ImageFilter
-from playsound import playsound
 from enemies import world_map
 
 
 print("privyet!! Comrade")
 
-im = Image.open('/home/o11q/Desktop/git clone/chto-takoe-stolitsa/mozer racha/put.jpg')
-print(im.format, im.size, im.mode)
+#im = Image.open('/home/o11q/Desktop/git clone/chto-takoe-stolitsa/mozer racha/put.jpg')
+#print(im.format, im.size, im.mode)
 
 comrade_user = input('enter your name : ') #NameError
 
-R_SPY = open('/home/o11q/Desktop/git clone/chto-takoe-stolitsa/mozer racha/names.txt','r')
+R_SPY = open('/home/o11q/Desktop/git clone/Stalina-Blitzkrieg/mozer racha/names.txt','r')
 
 i = 19 #fix STRING INDEX   
 
@@ -21,15 +19,13 @@ print("You know what ill call you " + R_SPY.readline(i))
 path = input("is it okay to call you "  + R_SPY.readline(i) + "my FKHIEND [YES]or[NO] : ").upper() #fix \n
 
 #ADD A LOOP
-if(path == 'NO'): 
-     print("SYKAA!! GET OUT U FUCKING WESTERN SPY")
+if(path == 'NO'):
+        print("SYKAA!! GET OUT U FUCKING WESTERN SPY")
 elif(path == 'YES'):
     print("SO! my APYR what country  u want to invade today ?")
     enemy = input("choose your enemy : ").lower()
-     
-    def world_map(enemy):
-     
+    world_map(enemy) #enemies from enemies.py
 else:
-    print("you fucking donkey i said yes or not" + path)
-    playsound('audio.mp3') #ownage pranks ill slap ur ass like 3abas
+    print("you fucking donkey i said yes or not")
+    #playsound('audio.mp3') #ownage pranks ill slap ur ass like 3abas
 R_SPY.close() #close theTXT FILE
