eec4f17e075244baf031e94d8e7a19b6c6b9820f
Ahmed ELMOUNTASSIR <ahmedelmountassir10@gmail.com>
2020-01-08 14:55:16 +0100

    Update enemies.py

--- mozer racha/enemies.py
+++ mozer racha/enemies.py
@@ -4,13 +4,13 @@
 def world_map(enemy):
      if(enemy == 'morocco'):
           print("those are our APYR's u shouldnt attack them he")
-          playsound('audio.mp3')
+          #playsound('audio.mp3')
      elif(enemy == 'russia'):
           print("CYYYKA BLYAAAT! you are a fucking traitor KGB want to know your location")
-          playsound('audio.mp3')
+          #playsound('audio.mp3')
      elif(enemy == 'Afghanistan'):
           print("kha-kha-kha!ooh those freaking terrorists u'd better shoot them with the votkalachnikov")
-          playsound('audio.mp3')
+          #playsound('audio.mp3')
      elif(enemy == 'Australia'):
           print("")
      elif(enemy == 'Canada'):
