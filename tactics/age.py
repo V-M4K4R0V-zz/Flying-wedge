3b4f6eacf68363f84ce05d09d3c479d9a4687d2b
V-M4K4R0V <ahmedelmountassir10@gmail.com>
2020-01-09 14:22:21 +0100

    adding_more_func

---
+++ tactics/age.py
@@ -0,0 +1,5 @@
+from motherland import choosing_the_enemy
+
+def comrade_age(age):
+    if (age < 18)
+        print("you are " + age +" ar you sure")
--- tactics/motherland.py
+++ tactics/motherland.py
@@ -1,17 +1,22 @@
 #author : V-M4K4R0V
 #add music , graphics , national anthem
-#more dialogs
+
 from enemies import world_map
+from age import comrade_age
 
 
-print("privyet!! Comrade")
+print("Privyet!! Comrade")
 
 #im = Image.open('/home/o11q/Desktop/git clone/chto-takoe-stolitsa/mozer racha/put.jpg')
 #print(im.format, im.size, im.mode)
 
-comrade_user = input('enter your name : ') #NameError
+comrade_user = input('Enter your name : ') #NameError
+B_year = input('Enter your birth year : ')
+age = 2020 - int(B_year)
 
-R_SPY = open('/home/o11q/Desktop/git clone/Stalina-Blitzkrieg/mozer racha/names.txt','r')
+comrade_age(age) #ft_age
+
+R_SPY = open('/home/o11q/Desktop/git clone/Flying-wedge/tactics/names.txt','r')
 
 i = 19 #fix STRING INDEX   
 
@@ -19,13 +24,14 @@ print("You know what ill call you " + R_SPY.readline(i))
 path = input("is it okay to call you "  + R_SPY.readline(i) + "my FKHIEND [YES]or[NO] : ").upper() #fix \n
 
 #ADD A LOOP
-if(path == 'NO'):
-        print("SYKAA!! GET OUT U FUCKING WESTERN SPY")
-elif(path == 'YES'):
-    print("SO! my APYR what country  u want to invade today ?")
-    enemy = input("choose your enemy : ").lower()
-    world_map(enemy) #enemies from enemies.py
-else:
-    print("you fucking donkey i said yes or not")
-    #playsound('audio.mp3') #ownage pranks ill slap ur ass like 3abas
+def choosing_the_enemy(path):
+    if(path == 'NO'):
+            print("SYKAA!! GET OUT U FUCKING WESTERN SPY")
+    elif(path == 'YES'):
+        print("SO! my APYR what country  u want to invade today ?")
+        enemy = input("choose your enemy : ").lower()
+        world_map(enemy) #enemies from enemies.py
+    else:
+        print("you fucking donkey i said yes or not")
+        #playsound('audio.mp3') #ownage pranks ill slap ur ass like 3abas
 R_SPY.close() #close theTXT FILE
