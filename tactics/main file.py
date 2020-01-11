f1ee3420b16cd55dede6100c9b6cdea7935dab5b
V-M4K4R0V <ahmedelmountassir10@gmail.com>
2020-01-11 17:46:53 +0100

    remove-some-func

---
+++ tactics/main file.py
@@ -0,0 +1,19 @@
+#author : V-M4K4R0V
+#add music , graphics , national anthem
+from the_recipe import ask_usr
+import turtle
+#from tkinter import *
+
+background = "/home/o11q/Desktop/git clone/Flying-wedge/tactics/game-map/gamemap.gif"
+
+window = turtle.Screen()
+window.title("Flying Wedge")
+window.bgpic(background)
+window.setup(width=1300, height=800)
+#background.s
+#window.pensize(24)
+#window.stamp()
+window.tracer(0)
+
+while True:
+    window.update()
---
+++ tactics/the_recipe.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+from countries import world_map
+
+way1 = Path("pics")
+way2 = Path("national anthems")
+
+def ask_usr():
+    #names file
+    name = '/home/o11q/Desktop/git clone/Flying-wedge/tactics/names.txt'
+    
+    print("Privyet!! Comrade")
+    comrade_user = input('Enter your name : ')
+    B_year = input('Enter your birth year : ')
+    age = 2020 - int(B_year)
+    #age fuction from age.py
+    comrade_age(age)
+    
+    R_SPY = open(name,'r')
+    #fix STRING INDEX i
+    i = 19
+
+    print("You know what ill call you " + R_SPY.readline(i))
+    print("SO! what country  u want to know about?")
+    enemy = input("choose a country : ").lower()
+
+    #countries from countries.py
+    world_map(enemy)
+    
+    #close the names close
+    R_SPY.close()
