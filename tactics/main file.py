1ac53f41764cae44012b51af587009231054f9f2
V-M4K4R0V <ahmedelmountassir10@gmail.com>
2020-01-13 14:35:24 +0100

    test-tk-win

--- tactics/main file.py
+++ tactics/main file.py
@@ -4,16 +4,16 @@ from the_recipe import ask_usr
 from countries import world_map
 from age import comrade_age
 from tkinter import *
-from PIL import ImageTK,Image
+#from PIL import ImageTK,Image
 
-background = "/home/o11q/Desktop/git clone/Flying-wedge/tactics/game-map/gamemap.gif"
+#background = "/home/o11q/Desktop/git clone/Flying-wedge/tactics/game-map/gamemap.gif"
 
 root=Tk()
 
-convas=convas(root,width=1200,height=800)
-image=ImageTK.PhotoImage(Image.open(background))
+#convas=convas(root,width=1200,height=800)
+#image=ImageTK.PhotoImage(Image.open(background))
 
-convas.create_image(0,0,anchor=NW,image=image)
+#convas.create_image(0,0,anchor=NW,image=image)
 convas.pack()
 
 root.mainloop()
