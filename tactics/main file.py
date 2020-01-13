7e33e113dd1bca09e0b0669c37fdaf8a4721acdc
V-M4K4R0V <ahmedelmountassir10@gmail.com>
2020-01-13 17:27:07 +0100

    add-menu

--- tactics/main file.py
+++ tactics/main file.py
@@ -5,11 +5,28 @@ from countries import world_map
 from age import comrade_age
 from tkinter import *
 
-root=Tk()
+#window
+root = Tk()
+def hello():
+    print("welcome comrad")
 
-canvas = Canvas(width=1400, height=900, bg='blue')
+# create a toplevel menu
+menubar = Menu(root)
+menubar.add_command(label="Hello!", command=hello)
+menubar.add_command(label="Quit!", command=root.quit)
+
+# display the menu
+root.config(menu=menubar)
+
+#map
+canvas = Canvas(width=1366, height=768, bg='blue')
 background = PhotoImage(file="C:\\Users\\ahmed\\Desktop\\work\\Flying-wedge\\tactics\\game-map\\new-map.png")
 canvas.create_image(0,0, image=background, anchor=NW)
 canvas.pack()
+#user info
+#ask_usr(nana)
+#Coordinates
+#Coordinates = look for a Coordinates LIB
+#world_map(coordinates)
 
 root.mainloop()
