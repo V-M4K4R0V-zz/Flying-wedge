ac8183a09f05bb52fbdb796c2be001290bbf86c3
V-M4K4R0V <ahmedelmountassir10@gmail.com>
2020-01-13 17:48:48 +0100

    add-top-menu

---
+++ tactics/about.py
@@ -0,0 +1,5 @@
+def about_game():
+    print("welcome !!player")
+    print("this an educational game made by @V-M4K4ROV")
+    print("An educational game about the history of the modern world.\nYou can choose a specific decade to see what is happening in a particular country .")
+    
--- tactics/main file.py
+++ tactics/main file.py
@@ -3,17 +3,36 @@
 from the_recipe import ask_usr
 from countries import world_map
 from age import comrade_age
+from about import about_game
 from tkinter import *
 
 #window
 root = Tk()
 def hello():
-    print("welcome comrad")
+    print("welcome Player!")
 
-# create a toplevel menu
 menubar = Menu(root)
-menubar.add_command(label="Hello!", command=hello)
-menubar.add_command(label="Quit!", command=root.quit)
+
+#File button
+filemenu = Menu(menubar, tearoff=0)
+menubar.add_cascade(label="File", menu=filemenu)
+filemenu.add_command(label="Open", command=hello)
+filemenu.add_command(label="Save", command=hello)
+filemenu.add_separator()
+filemenu.add_command(label="Exit", command=root.quit)
+
+# create more pulldown menus // edit button
+editmenu = Menu(menubar, tearoff=0)
+menubar.add_cascade(label="Edit", menu=editmenu)
+editmenu.add_command(label="Cut", command=hello)
+editmenu.add_command(label="Copy", command=hello)
+editmenu.add_command(label="Paste", command=hello)
+
+#help button
+helpmenu = Menu(menubar, tearoff=0)
+menubar.add_cascade(label="Help", menu=helpmenu)
+helpmenu.add_command(label="About", command=about_game)
+
 
 # display the menu
 root.config(menu=menubar)
@@ -23,10 +42,5 @@ canvas = Canvas(width=1366, height=768, bg='blue')
 background = PhotoImage(file="C:\\Users\\ahmed\\Desktop\\work\\Flying-wedge\\tactics\\game-map\\new-map.png")
 canvas.create_image(0,0, image=background, anchor=NW)
 canvas.pack()
-#user info
-#ask_usr(nana)
-#Coordinates
-#Coordinates = look for a Coordinates LIB
-#world_map(coordinates)
-
+#game loop
 root.mainloop()
